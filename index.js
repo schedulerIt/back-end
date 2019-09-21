62e34c4e0fb48524a3e4d3084736bccf186383f4
Ernesto Pena <ernestopenad@gmail.com>
2019-09-20 23:42:35 -0500

    Finding a solution for Heroku

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const server = require('./server/server.js');
 //DECLARING DOTENV 
 require('dotenv').config();
 
-const PORT = process.env.SERVER_PORT || 4000;
-const HOST = process.env.SERVER_HOST;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT , () => {
     console.log(`Server Listening on port ${PORT}`);

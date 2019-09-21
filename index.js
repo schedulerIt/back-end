29e60652236e2a5a252f714841a9eeb621553184
Ernesto Pena <ernestopenad@gmail.com>
2019-09-20 22:58:59 -0500

    Removed the host variable from index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require('./server/server.js');
 const PORT = process.env.SERVER_PORT || 4000;
 const HOST = process.env.SERVER_HOST;
 
-server.listen(PORT , HOST, (req , res) => {
+server.listen(PORT , (req , res) => {
     console.log(`Server Listening on port ${PORT}`);
 })
 

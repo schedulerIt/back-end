dce72d5d4d15c39d4e6425530da6d12c02b91ab5
Ernesto Pena <ernestopenad@gmail.com>
2019-09-25 17:18:46 -0500

    GET end point for all accounts

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const server = express();
 
 server.use(express.json());
 
+//ROUTER DECLARATIONS
+const accountRouter = require('../accounts/router.js');
+
+server.use('/accounts', accountRouter);
+
+
+
+
 server.get('/', (req , res) => {
     res.status(200).json({ Server:'This is my back end for Scheduller'} );
 })
